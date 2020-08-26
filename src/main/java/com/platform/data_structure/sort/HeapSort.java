0d8eae19f7e81dc2fa9ab25a2c2fe8a7d67d6e1a
czzju76 <czzju01@163.com>
2020-08-26 08:16:03 +0800

    新增快速排序、归并排序、堆排序

---
+++ src/main/java/com/platform/data_structure/sort/HeapSort.java
@@ -0,0 +1,47 @@
+package com.platform.data_structure.sort;
+
+/**
+ * @Title: BubbleSort
+ * @Description: 堆排序，时间复杂度O(N*LogN)，额外空间复杂度O(1)
+ * @Auther:Lyon Chen
+ * @Version: 1.0
+ * @create 2020/8/25 14:51
+ */
+public class HeapSort extends Sort {
+
+    public static void sort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            heapInsert(arr, i);
+        }
+        int size = arr.length;
+        swap(arr, 0, --size);
+        while (size > 0) {
+            heapify(arr, 0, size);
+            swap(arr, 0, --size);
+        }
+    }
+
+    public static void heapInsert(int[] arr, int index) {
+        while (arr[index] > arr[(index - 1) / 2]) {
+            swap(arr, index, (index - 1) / 2);
+            index = (index - 1) / 2;
+        }
+    }
+
+    public static void heapify(int[] arr, int index, int size) {
+        int left = index * 2 + 1;
+        while (left < size) {
+            int largest = left + 1 < size && arr[left + 1] > arr[left] ? left + 1 : left;
+            largest = arr[largest] > arr[index] ? largest : index;
+            if (largest == index) {
+                break;
+            }
+            swap(arr, largest, index);
+            index = largest;
+            left = index * 2 + 1;
+        }
+    }
+}
---
+++ src/main/java/com/platform/data_structure/sort/MergeSort.java
@@ -0,0 +1,47 @@
+package com.platform.data_structure.sort;
+
+/**
+ * @Title: BubbleSort
+ * @Description: 归并排序，时间复杂度O(N*LogN)，额外空间复杂度O(N)
+ * @Auther:Lyon Chen
+ * @Version: 1.0
+ * @create 2020/8/25 14:51
+ */
+public class MergeSort {
+
+    public static void sort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        mergeSort(arr, 0, arr.length - 1);
+    }
+
+    public static void mergeSort(int[] arr, int l, int r) {
+        if (l == r) {
+            return;
+        }
+        int mid = l + ((r - l) >> 1);
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid + 1, r);
+        merge(arr, l, mid, r);
+    }
+
+    public static void merge(int[] arr, int l, int m, int r) {
+        int[] help = new int[r - l + 1];
+        int i = 0;
+        int p1 = l;
+        int p2 = m + 1;
+        while (p1 <= m && p2 <= r) {
+            help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        while (p1 <= m) {
+            help[i++] = arr[p1++];
+        }
+        while (p2 <= r) {
+            help[i++] = arr[p2++];
+        }
+        for (i = 0; i < help.length; i++) {
+            arr[l + i] = help[i];
+        }
+    }
+}
